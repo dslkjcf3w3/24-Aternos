66706c5488b8b9afe99a863f44b8e34b0e44b6ab
dslkjcf3w3 <103445923+dslkjcf3w3@users.noreply.github.com>
2022-04-11 19:39:45 +0300

    Update bot.js

--- JS/bot.js
+++ JS/bot.js
@@ -4,13 +4,13 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
+    host: "topcrafthub.aternos.me",
     port: "25565",
     username: "Test",
     version: false
     })
     bot.on('login', function() {
-      bot.chat('/reginster 123123123 123123123')
+      bot.chat('/register 123123123')
     })
     bot.on('chat', (username, message) => {
       if (username === bot.username) return
